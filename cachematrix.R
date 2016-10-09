033d2fd4e7f076b9db913e27b5816ff20a6643c3
callmegramma <plamen.iliev.ivanov@gmail.com>
2016-10-09 22:07:08 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function are identical to the example, deleated the set function within the "matrix" object function, as it serves no pratical purpose in this assigmnet
 
-## Write a short comment describing this function
+## This function stores an input matrix "x" and creates an object "m" to store the inverse of "x" 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## This function inverts a matrix and caches it's inverse for future reference or returns a cached inverse if available
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
